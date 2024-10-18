46a7bd2827f27fbe5436f2b241e8d088173399c8
carlyjohnsony <nipsykay48@gmail.com>
2024-10-18 05:11:02 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "260776642930"
+const SESSION_ID = "PRABATH-MD~SGAWHAAB#dr8g890crcK7P3yIkHMI5nNkni8hcbE5U6QUZ4oIyAw"
 
 
 module.exports = {
